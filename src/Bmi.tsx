f35332a6a42346490b0a024336d3d9b24ab2f2f6
kinoshita0623 <kuutarousii@gmail.com>
2021-01-22 20:19:40 +0900

    change Bmiをhooksで書き換えた

--- src/Bmi.tsx
+++ src/Bmi.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 
-interface State {
-    height: number,
-    weight: number,
-    bmi: number,
+
+
+export default function Bmi() {
+    const [height, setHeight] = useState<number>(0);
+    const [weight, setWeight] = useState<number>(0);
+    const [bmi, setBmi] = useState<number>(0);
+
+    const handleHeightInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let h = Number(e.target.value);
+        executeBmi(h, weight);
+    }
+
+    const handleWeightInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let w = Number(e.target.value);
+        executeBmi(height, w);
+    }
+
+    const executeBmi = (height: number, weight: number)=>{
+        setHeight(height);
+        setWeight(weight);
+        let bmi = weight / Math.pow(height / 100, 2);
+        setBmi(bmi);
+    }
+    
+    return (
+        <div>
+            <h1>BMI計算</h1>
+            <div>
+                身長:<input type="text" onChange={handleHeightInput} />cm
+                </div>
+            <div>
+                体重:<input type="text" onChange={handleWeightInput} />kg
+                </div>
+            <div>BMI:{bmi}</div>
+
+        </div>
+    );
 }
 
-class Bmi extends React.Component<{}, State> {
+/*class Bmi extends React.Component<{}, State> {
 
     constructor(props: any) {
         super(props);
@@ -55,3 +89,4 @@ class Bmi extends React.Component<{}, State> {
 }
 
 export default Bmi;
+*/
